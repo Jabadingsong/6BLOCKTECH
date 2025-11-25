84ee8370057cd9150cd4c8cdbe948b75a80c02bd
Ezkylle Opiniano <159036351+Jabadingsong@users.noreply.github.com>
2025-11-25 13:56:10 +0800

    Add files via upload
    November 25, 2025 13:57 Philippine Time

---
+++ prelim/Solidity Basisc 2/Constants.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.20;
+
+contract Constants {
+
+    address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint public constant MY_UINT = 123;
+
+}
+
+// contract Var {
+//     address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+// }
